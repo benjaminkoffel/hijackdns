bef7c0b4bea545d51aeb2aac63eff58039c97169
Benjamin Koffel <benjamink@beteasy.com.au>
2017-03-17 01:21:08 +1100

    add retry on timeout logic

--- hijackdns.py
+++ hijackdns.py
@@ -6,21 +6,21 @@ import dns.exception
 import argparse
 import socket
 
-def list_authoritative_nameservers(nameserver, domain):
+def list_authoritative_nameservers(nameservers, domain):
     resolver = dns.resolver.Resolver()
-    resolver.nameservers=[socket.gethostbyname(nameserver)]
+    resolver.nameservers = nameservers
     resolver.timeout = 5
     resolver.lifetime = 5
-    nameservers = []
+    authoritative_nameservers = []
     for rdata in resolver.query(domain, 'NS'):
-        nameservers.append(rdata.target.to_text())
-    return nameservers
+        authoritative_nameservers.append(rdata.target.to_text())
+    return authoritative_nameservers
 
-def check_ns_record(nameserver, domain):
+def check_ns_record(nameservers, domain, attempts=3):
     resolver = dns.resolver.Resolver()
-    resolver.nameservers=[socket.gethostbyname(nameserver)]
-    resolver.timeout = 5
-    resolver.lifetime = 5
+    resolver.nameservers = nameservers
+    resolver.timeout = 1
+    resolver.lifetime = 1
     try:
         resolver.query(domain, 'NS')
     except dns.exception.DNSException as e:
@@ -31,14 +31,17 @@ def check_ns_record(nameserver, domain):
         elif 'None of DNS query names exist' in e.msg:
             return 'NOTFOUND'
         elif 'timed out' in e.msg:
-            return 'TIMEOUT'
+            if attempts > 0:
+                check_ns_record(nameservers, domain, attempts - 1)
+            else:
+                return 'TIMEOUT'
         else:
             return 'ERROR: ' + e.msg
     return 'OK'
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-t', '--target-domain', help='target base domain')
-parser.add_argument('-p', '--public-dns', help='public DNS server', default='8.8.8.8')
+parser.add_argument('-p', '--public-dns', help='public DNS', default='8.8.8.8')
 parser.add_argument('-s', '--subdomain-list', help='file containing a list of subdomains')
 args = parser.parse_args()
 
@@ -46,21 +49,21 @@ with open(args.subdomain_list) as list_file:
     subdomains = list_file.readlines()
 subdomains = [x.strip() for x in subdomains]
 
+public_nameservers = [args.public_dns]
+
 print('authoritative nameservers:')
-authoritative_nameservers = list_authoritative_nameservers(args.public_dns, args.target_domain)
+authoritative_nameservers = list_authoritative_nameservers(public_nameservers, args.target_domain)
 for nameserver in authoritative_nameservers:
     print(nameserver)
 print('')
 
-subdomains.insert(0, '_example.dev')
-
 for subdomain in subdomains:
     domain = subdomain + '.' + args.target_domain
     print(domain, end=' ')
-    public_ns_status = check_ns_record(args.public_dns, domain)
+    public_ns_status = check_ns_record(public_nameservers, domain)
     print(public_ns_status, end=' ')
     if public_ns_status == 'SERVFAIL':
-        authoritative_ns_status = check_ns_record(authoritative_nameservers[0], domain)
+        authoritative_ns_status = check_ns_record(authoritative_nameservers, domain)
         print(authoritative_ns_status, end=' ')
         if authoritative_ns_status == 'INNS':
             print('--- VULNERABLE ---', end='')
